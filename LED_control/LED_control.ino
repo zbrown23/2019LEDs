ed61daf2d4c4a58d7dd86aa4dec21a384e913b5a
Zach Brown <21brownz@da.org>
2019-02-22 17:35:48 -0500

    initial commit

---
+++ LED_control/LED_control.ino
@@ -0,0 +1,111 @@
+/*
+ * LED Animations
+ * 1. DARC SIDE Colors
+ * 2. Chase up Elevator
+ * 3. Pulsing Red
+ * 4. Pulsing Blue
+ * 5. Solid Red
+ * 6. Solid Blue
+ */
+
+//includes
+#include <Wire.h>
+#include <FastLED.h>
+
+//Changeable variables
+#define DATA_PIN 2
+#define BRIGHTNESS 100
+int fadeAmount = 5;
+int wait = 50;
+const int leftBottom = 37;
+const int rightBottom = 37;
+const int frontBottom = 0;
+const int leftElevator = 0;
+const int rightElevator = 0;
+
+//definitions so the code knows what it's doing
+//#define STRAND_LENGTH leftBottom+rightBottom+frontBottom+leftElevator+rightElevator
+#define STRAND_LENGTH 37
+int brightness = BRIGHTNESS;
+CRGB leds[STRAND_LENGTH];
+
+void setup() {
+  //setup code
+  Wire.begin(8);
+  FastLED.addLeds<WS2812B, DATA_PIN, GRB>(leds, STRAND_LENGTH).setCorrection(TypicalPixelString);
+  FastLED.setBrightness(BRIGHTNESS);
+  FastLED.show();
+}
+
+void loop() {
+     elevatorChaseRed();
+    }
+
+
+void CoralChaseBackForth() {
+        for(int dot = 0; dot < STRAND_LENGTH; dot++) { 
+            leds[dot+0].setRGB( 200, 75, 90);
+            leds[dot+1].setRGB( 200, 75, 90);
+            leds[dot+2].setRGB( 200, 75, 90);
+            leds[dot+3].setRGB( 200, 75, 90);
+            leds[dot+4].setRGB( 200, 75, 90);
+            FastLED.show();
+        leds[dot] = 0x000000;
+            delay(wait);
+  }
+        for(int dot = 0; dot< STRAND_LENGTH; dot--) {
+            leds[dot+0].setRGB( 200, 75, 90);
+            leds[dot+1].setRGB( 200, 75, 90);
+            leds[dot+2].setRGB( 200, 75, 90);
+            leds[dot+3].setRGB( 200, 75, 90);
+            leds[dot+4].setRGB( 200, 75, 90);
+            FastLED.show();
+        leds[dot] = 0x000000;
+            delay(wait);
+        }
+}
+
+void colorWipeRed() {
+  for(int i=0; i<STRAND_LENGTH; i++) {
+    leds[i].setRGB(128, 0, 0);
+    FastLED.show();
+    delay(wait);
+  }
+}
+
+void colorWipeBlue() {
+  for(int i=0, j=0; i<STRAND_LENGTH; i++) {
+    leds[i].setRGB(0, 0, 128);
+    FastLED.show();
+    delay(wait);
+  }
+}
+
+void colorWipeCoral() {
+  for(int i=0; i<STRAND_LENGTH; i++) {
+    leds[i].setRGB(200, 75, 90);
+    FastLED.show();
+    delay(wait);
+  }
+}
+
+void colorWipeBlack() {
+  for(int i=0; i<STRAND_LENGTH; i++) {
+    leds[i] = CRGB::Black;
+    FastLED.show();
+    delay(wait);
+  }
+}
+
+void elevatorChaseRed() {
+  for(int dot = 0; dot < STRAND_LENGTH; dot+2) { 
+            leds[dot].setRGB(128, 0, 0);
+            FastLED.show();
+        leds[dot] = CRGB::Black;
+            delay(wait);
+  }
+}
+
+void pulseRed() {
+
+ }
